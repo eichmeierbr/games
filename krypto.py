a2704e7c5e8fea49ef69267a0b6dd10576706e89
eichmeierbr <eichmeierbr@gmail.com>
2019-05-22 20:53:00 -0600

    Update Error Control. Need to alter for double digit numbers. Add Feedback

--- krypto.py
+++ krypto.py
@@ -2,26 +2,55 @@
 
 import random
 
-deck = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+deck = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9]#, 10, 10, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15, 16, 16, 17, 17, 18, 19, 20, 21, 22, 23, 24, 25]
 
-def checkSolution(input, goal):
-    pass
+def usesFullHand(input, hand_true):
+    guessHand=[]
+    hand = hand_true[:]
+    # Extract all digits from input string .... This part breaks with double digit numbers
+    for char in input:
+        if(char.isdigit()):
+            guessHand.append(int(char,10))
+
+    # Sort lists to facilitate comparison
+    hand.sort()
+    guessHand.sort()
+
+    if guessHand == hand:
+        return True
+    else:
+        informWhyBad(guessHand, hand)
+        return False
+    
+    
+def informWhyBad(guess, hand):
+    print '\nYou entered an invalid input'
+
+    if not len(guess)==len(hand):
+        print 'Your input has %i numbers\n' %(len(guess))
+    else:
+        print 'Your input uses numbers not found in the hand\n'
+
 
 def displayInfo(hand, goal):
     print 'Achieve:\t %i' %(goal)
     print 'Using:\t ',hand
 
-def requestInput(hand):
+
+def requestSolution(hand):
     response = raw_input('Please input your solution: ')
     return response
     
 
 
 def getInput(hand, goal):
-    displayInfo(hand, goal)
-    response = requestInput(hand)
-    # Add Error Handling on Response
+    haveGoodInput = False
+    while not haveGoodInput:
+        displayInfo(hand, goal) # Print hand and goal
+        response = requestSolution(hand) # Obtain input from user
+        haveGoodInput = usesFullHand(response, hand) # Check if input is good
     return response
+
 
 def initiateRound():
     deck_copy = random.shuffle(deck)
@@ -37,10 +66,9 @@ def turn():
 
     while (not solved):
         response = getInput(hand, goal)
-        response
         solved = (eval(response) == goal)
         if(not solved):
-            print 'That is incorrect. Try again!'
+            print 'That is Incorrect. Try again!\n'
 
     print 'That is Correct! Congratulations!'
 
